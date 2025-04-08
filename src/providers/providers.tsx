6a027122cbd30c53e176217bac3ec5d7d5c27c74
Eugene <sifmeop@gmail.com>
2025-04-09 00:12:33 +0300

    return telegram provider

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -5,22 +5,23 @@ import isoWeek from 'dayjs/plugin/isoWeek'
 import utc from 'dayjs/plugin/utc'
 import { Toaster } from 'react-hot-toast'
 import { TanstackProvider } from './tanstack-provider'
+import { TelegramProvider } from './telegram-provider'
 
 dayjs.extend(isoWeek)
 dayjs.extend(utc)
 
 export const Providers = ({ children }: React.PropsWithChildren) => {
   return (
-    // <TelegramProvider>
-    <TanstackProvider>
-      <Toaster
-        position='top-center'
-        toastOptions={{
-          duration: 2500
-        }}
-      />
-      {children}
-    </TanstackProvider>
-    // </TelegramProvider>
+    <TelegramProvider>
+      <TanstackProvider>
+        <Toaster
+          position='top-center'
+          toastOptions={{
+            duration: 2500
+          }}
+        />
+        {children}
+      </TanstackProvider>
+    </TelegramProvider>
   )
 }
--- src/providers/telegram-provider.tsx
+++ src/providers/telegram-provider.tsx
@@ -31,5 +31,18 @@ export const TelegramProvider = ({ children }: React.PropsWithChildren) => {
     }
   }, [])
 
+  if (!isTMA()) {
+    return (
+      <div className='grid place-items-center h-dvh bg-white'>
+        <div className='flex flex-col items-center gap-1'>
+          <h1 className='text-xl font-bold'>Not Telegram App</h1>
+          <a href='https://t.me/sifthabits_bot' target='_blank' className='underline'>
+            Open in Telegram
+          </a>
+        </div>
+      </div>
+    )
+  }
+
   return <>{children}</>
 }
