b09e4ebe32965d4b48f5aa4db469b331d18e9d01
Eugene <sifmeop@gmail.com>
2025-04-11 19:34:27 +0300

    add close mini app if 403 error

--- src/providers/tanstack-provider.tsx
+++ src/providers/tanstack-provider.tsx
@@ -1,14 +1,28 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { closeMiniApp } from '@telegram-apps/sdk-react'
+import { AxiosError } from 'axios'
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
       refetchOnMount: false,
-      staleTime: Infinity
+      staleTime: Infinity,
+      retry: 1
     }
-  }
+  },
+  queryCache: new QueryCache({
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        const isForbiddenError = error.response && error.response.status === 403
+
+        if (isForbiddenError) {
+          closeMiniApp()
+        }
+      }
+    }
+  })
 })
 
 export const TanstackProvider = ({ children }: React.PropsWithChildren) => {
