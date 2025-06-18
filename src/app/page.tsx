6465462623528d75beede74b432165036411c8db
Eugene <sifmeop@gmail.com>
2025-06-18 12:43:04 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { redirect } from 'next/navigation'
-import { ROUTER_LINKS } from '~/constants/router-links'
-
 export default function Home() {
-  return redirect(ROUTER_LINKS.HABITS)
+  // return redirect(ROUTER_LINKS.HABITS)
+  return <div>Home</div>
 }
